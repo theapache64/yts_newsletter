6dafa2ea32117564c117bf130b424d51b95e0802
theapache64-alpha <theapache64@gmail.com>
2017-10-06 08:20:29 +0530

    .backup

--- src/com/theah64/yts_nl/NewsLetter.java
+++ src/com/theah64/yts_nl/NewsLetter.java
@@ -25,8 +25,7 @@ public class NewsLetter {
 
         public Builder() {
             sb
-                    .append("YTS Newsletter")
-                    .append("\n---------------");
+                    .append("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\" \"http://www.w3.org/TR/REC-html40/loose.dtd\"> <html style=\"margin: 0 auto; padding: 0;\"> <head> </head> <body style=\"background-color: #171717; margin: 0 auto; padding: 0;\" bgcolor=\"#171717\"> <div id=\"header\" style=\"width: 100%; height: 80px; border-bottom-width: 1px; border-bottom-color: #2f2f2f; border-bottom-style: solid; background-color: #1d1d1d; margin: 0 auto; padding: 0;\"> <img id=\"yts_logo\" src=\"yts_logo.png\" style=\"margin: 0 auto; padding: 22px;\"> </div> <div id=\"body\" style=\"text-align: center; color: white; height: 158px; margin: 0 auto; padding: 50px;\" align=\"center\"> <p style=\"font-weight: 600; color: #4a4a4a; font-size: 20px; font-family: 'Roboto', sans-serif; margin: 0 auto; padding: 0;\">20 new movies found</p> <br style=\"margin: 0 auto; padding: 0;\"> <br style=\"margin: 0 auto; padding: 0;\"> <!--Movies--> <div id=\"movies\" style=\"margin: 0 auto; padding: 0;\">");
         }
 
         /**
