dac6b2c183bebdb49b716313f1686b069ecc8e43
theapache64-charlie <theapache64@gmail.com>
2017-12-23 19:49:25 +0530

    .backup

--- webengine/src/com/theah64/webengine/utils/RandomString.java
+++ webengine/src/com/theah64/webengine/utils/RandomString.java
@@ -1,6 +1,7 @@
 package com.theah64.webengine.utils;
 
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by theapache64 on 9/4/16.
@@ -9,7 +10,8 @@ public class RandomString {
 
 
     private static final String randomEngine = "0123456789AaBbCcDdEeFfGgHhIiJjKkLkMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
-    private static Random random;
+    private static Random random = new Random();
+    ;
 
     public static String get(final int length) {
         return getRandomString(length);
@@ -17,9 +19,7 @@ public class RandomString {
 
 
     private static String getRandomString(final int length) {
-        if (random == null) {
-            random = new Random();
-        }
+
         final StringBuilder apiKeyBuilder = new StringBuilder();
         for (int i = 0; i < length; i++) {
             apiKeyBuilder.append(randomEngine.charAt(random.nextInt(randomEngine.length())));
@@ -32,4 +32,31 @@ public class RandomString {
     }
 
 
+    public static String getRandomNumber(long length) {
+        final StringBuilder numBuilder = new StringBuilder();
+        System.out.println(length % 18);
+        long loopCount = length / 18;
+        for (int i = 0; i < loopCount; i++) {
+            numBuilder.append(getRandomNumberLimited(18));
+        }
+        numBuilder.append(getRandomNumberLimited((int) length % 18));
+        return numBuilder.toString();
+    }
+
+    private static String getRandomNumberLimited(int length) {
+
+        if (length < 1) {
+            throw new IllegalArgumentException("Length must be > 0");
+        }
+
+        if (length > 18) {
+            throw new IllegalArgumentException("Length must be < 19");
+        }
+
+
+        String sLowerLimit = length == 1 ? "1" : (1 + String.format("%0" + (length - 1) + "d", 0));
+        final long lowerLimit = Long.parseLong(sLowerLimit);
+        final long upperLimit = Long.parseLong(sLowerLimit.replaceAll("\\d", "9"));
+        return String.valueOf(ThreadLocalRandom.current().nextLong(lowerLimit, upperLimit + 1));
+    }
 }
